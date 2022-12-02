3e36d2377b94f9c6e7b0f46d03faeaa48f51ffac
Mateusz Blyszcz <mat@superformula.com>
2022-12-02 11:05:10 +0100

    use layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
+import Layout from '../components/layout';
 
 
 const AboutPage = () => {
   return (
-    <main>
+    <Layout>
       <h1>About me</h1>
-    </main>
+    </Layout>
   )
 }
 

99748fb4da9b5d37e72be7fd4091e3307ba083fc
Mateusz Blyszcz <mat@superformula.com>
2022-12-02 11:50:03 +0100

    read mdx and display all posts from /blog directory in one page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,20 +6,34 @@ import Seo from '../components/seo'
 const BlogPage = ({data}) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-      {data.allFile.nodes.map(({name}) => <li>{name}</li>)}
-      </ul>
+      
+      {data.allMdx.nodes.map((node) => <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </article>)}
+      
     </Layout>
   )
 }
 
 export const query = graphql`
   query {
-      allFile {
-        nodes {
-          name
+    allMdx(sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        parent {
+          ... on File {
+            modifiedTime(formatString: "MMMM D, YYYY")
+          }
         }
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        excerpt
       }
+    }
   }
 `
 
